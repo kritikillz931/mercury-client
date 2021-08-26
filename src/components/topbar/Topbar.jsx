a274091000037419baadca5ba2f68a3f42b87606
Justin <justinadams758@gmail.com>
2021-08-26 17:07:57 -0500

    app setup and starting development

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./topbar.css"
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="topbarwrapper">
+                <div className="topLeft">left</div>
+                <div className="topRight">right</div>
+            </div>
+        </div>
+    )
+}
