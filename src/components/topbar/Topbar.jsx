e3a7d5d552f64120f08591986f7041cc49175cad
Justin <justinadams758@gmail.com>
2021-08-27 15:41:48 -0500

    navbar complete

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import "./topbar.css"
+import mercurylogo from "../images/mercurylogo.png"
+import {NotificationsNone, Language, Settings} from '@material-ui/icons/';
 
 export default function Topbar() {
     return (
         <div className="topbar">
             <div className="topbarwrapper">
-                <div className="topLeft">left</div>
-                <div className="topRight">right</div>
+                <div className="topLeft">
+                    <span className="logo"><img className="mercuryImg" src={mercurylogo}/></span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                    <NotificationsNone/>
+                    <span className="topIconBadge">2</span>
+                    </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                    <Language/>
+                    <span className="topIconBadge">2</span>
+                    </div>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                    <Settings/>
+                    </div>
+                    <img src="https://static.wixstatic.com/media/5b4abc_c79574d4d9c84d02b74dd4ae6f7f8d25~mv2.jpg/v1/fill/w_560,h_682,al_c,q_90/5b4abc_c79574d4d9c84d02b74dd4ae6f7f8d25~mv2.webp" alt="" className="topAvatar" />
+                </div>
+                </div>
             </div>
         </div>
     )
