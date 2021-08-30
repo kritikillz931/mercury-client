b4eec0d6f82d8f0740010a0a9d2735262714e557
Justin <justinadams758@gmail.com>
2021-08-29 20:01:45 -0500

    added new members small widget

---
+++ src/components/widgetsSmall/widgetsSmall.jsx
@@ -0,0 +1,67 @@
+import { Visibility } from "@material-ui/icons"
+import "./widgetsSmall.css"
+
+export const WidgetsSmall = () => {
+    return (
+        <div className="widgetsSmall">
+            <span className="widgetsSmallTitle">New Members</span>
+            <ul className="widgetsSmallList">
+                <li className="widgetsSmallListItem">
+                    <img src="https://iveystudio.com/wp-content/uploads/2017/06/08-8019-pp_gallery/Christina-Pham-1%28pp_w1200_h1500%29.jpg" alt="" className="widgetsSmallImg" />
+                    <div className="widgetsSmallUser">
+                        <span className="widgetsSmallUsername">Anna Keller</span>
+                        <span className="widgetsSmallUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetsSmallButton">
+                        <Visibility className="widgetsSmallIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetsSmallListItem">
+                    <img src="https://iveystudio.com/wp-content/uploads/2017/06/08-8019-pp_gallery/Christina-Pham-1%28pp_w1200_h1500%29.jpg" alt="" className="widgetsSmallImg" />
+                    <div className="widgetsSmallUser">
+                        <span className="widgetsSmallUsername">Anna Keller</span>
+                        <span className="widgetsSmallUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetsSmallButton">
+                        <Visibility className="widgetsSmallIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetsSmallListItem">
+                    <img src="https://iveystudio.com/wp-content/uploads/2017/06/08-8019-pp_gallery/Christina-Pham-1%28pp_w1200_h1500%29.jpg" alt="" className="widgetsSmallImg" />
+                    <div className="widgetsSmallUser">
+                        <span className="widgetsSmallUsername">Anna Keller</span>
+                        <span className="widgetsSmallUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetsSmallButton">
+                        <Visibility className="widgetsSmallIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetsSmallListItem">
+                    <img src="https://iveystudio.com/wp-content/uploads/2017/06/08-8019-pp_gallery/Christina-Pham-1%28pp_w1200_h1500%29.jpg" alt="" className="widgetsSmallImg" />
+                    <div className="widgetsSmallUser">
+                        <span className="widgetsSmallUsername">Anna Keller</span>
+                        <span className="widgetsSmallUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetsSmallButton">
+                        <Visibility className="widgetsSmallIcon"/>
+                        Display
+                    </button>
+                </li>
+                <li className="widgetsSmallListItem">
+                    <img src="https://iveystudio.com/wp-content/uploads/2017/06/08-8019-pp_gallery/Christina-Pham-1%28pp_w1200_h1500%29.jpg" alt="" className="widgetsSmallImg" />
+                    <div className="widgetsSmallUser">
+                        <span className="widgetsSmallUsername">Anna Keller</span>
+                        <span className="widgetsSmallUserTitle">Software Engineer</span>
+                    </div>
+                    <button className="widgetsSmallButton">
+                        <Visibility className="widgetsSmallIcon"/>
+                        Display
+                    </button>
+                </li>
+            </ul>
+        </div>
+    )
+}
