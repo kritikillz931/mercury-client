5840dbfdf1f4795f5d1d2da12be752d5436c58d8
Justin <justinadams758@gmail.com>
2021-09-14 18:07:45 -0500

    top 3 products finished

--- src/components/widgetsLarge/widgetsLarge.jsx
+++ src/components/widgetsLarge/widgetsLarge.jsx
@@ -6,16 +6,36 @@ import "./widgetsLarge.css"
 
 export const WidgetsLarge = () => {
     const { transaction, getTransactions } = useContext(TransactionContext)
+    const { product, getProducts } = useContext(ProductContext)
     useEffect(() => {
         getTransactions()
+        .then(getProducts)
     }, [])
 
     let completedTransactions = transaction.filter(t => t.quantitySold >= 1)
     
-    let sortedTransactions = completedTransactions.sort(function(a,b){
+    
+    
+
+
+    let productSaleTotals = []
+    product.forEach(item => {
+        let sum = 0;
+        transaction.forEach(trans => {
+            console.log(trans)
+            if (trans.product.id === item.id) {
+                sum += trans.priceSold * trans.quantitySold
+            }
+        })
+        productSaleTotals.push({"image": item.image, "name":item.name, "revenue": sum})
+    })
+    console.log(productSaleTotals)
+
+    let sortedTransactions = productSaleTotals.sort(function(a,b){
         return b.quantitySold - a.quantitySold
     })
     let topThree = sortedTransactions.slice(0,3)
+    console.log(topThree)
 
 
     return (
@@ -25,7 +45,7 @@ export const WidgetsLarge = () => {
                 <table className="widgetsLargeTable">
                     <tr className="widgetLargeTr">
                         <th className="widgetsLargeTh">Product</th>
-                        <th className="widgetsLargeTh">Amount Sold</th>
+                        <th className="widgetsLargeTh">Revenue</th>
                     </tr>
 
 {   topThree.map(t => {
@@ -38,11 +58,11 @@ export const WidgetsLarge = () => {
                  return   <tr className="widgetLargeTr">
                         <td className="widgetsLargeUser">
                             
-                            <img src={t.product.image}
+                            <img src={t.image}
                             className="widgetsLargeImg" />
-                            <span className="widgetLargeName">{t.product.name}</span>
+                            <span className="widgetLargeName">{t.name}</span>
                         </td> 
-                            <td className="widgetsLargeAmount">{t.quantitySold}</td>
+                            <td className="widgetsLargeAmount">{t.revenue}</td>
                     </tr>                         
 })}
                 </table>
